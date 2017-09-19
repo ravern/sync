f191f261c06fc3b4cc3d9e666d5fd0171ff0ab4b
Ravern Koh <ravern.koh.dev@gmail.com>
2017-09-19 23:03:17 +0800

    Fixed wrong region for s3 bucket

--- config/prod.exs
+++ config/prod.exs
@@ -32,11 +32,19 @@ config :logger, level: :info
 # Arc use S3
 config :arc,
   storage: Arc.Storage.S3, # or Arc.Storage.Local
-  bucket: {:system, "AWS_S3_BUCKET"} # if using Amazon S3
+  bucket: {:system, "AWS_S3_BUCKET"}, # if using Amazon S3
+  virtual_host: true
 
 config :ex_aws,
   access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
-  secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role]
+  secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role],
+  region: "ap-southeast-1",
+  host: "s3.ap-southeast-1.amazonaws.com",
+  s3: [
+    scheme: "https://",
+    host: "s3.ap-southeast-1.amazonaws.com",
+    region: "ap-southeast-1",
+  ]
 
 # ## SSL Support
 #
